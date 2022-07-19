26f57ef8d5ad91b4caaaed2da0c135224c5fe402
Jaskaran Singh Bhalla <68080441+ultimatejaskaran@users.noreply.github.com>
2022-07-19 22:32:27 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -12,26 +12,41 @@ function rollDice2(img){
     switch(a){
         case 1:
             document.querySelector(img).setAttribute("src","dice1.png");
+            return a;
             break;
         case 2:
             document.querySelector(img).setAttribute("src","dice2.png");
+            return a;
             break;
         case 3:
             document.querySelector(img).setAttribute("src","dice3.png");
+            return a;
             break;
         case 4:
             document.querySelector(img).setAttribute("src","dice4.png");
+            return a;
             break;
         case 5:
             document.querySelector(img).setAttribute("src","dice5.png");
+            return a;
             break;
         case 6:
             document.querySelector(img).setAttribute("src","dice6.png");
+            return a;
             break;
     }
 }
-rollDice2(".img1");
-rollDice2(".img2");
+var randomNumber1 = rollDice2(".img1");
+var randomNumber2 = rollDice2(".img2");
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
 }
 
 
